7d574eb51b5e557a0f290031abf0ea1a01f357af
Kevin Smith <kevin@edge-plus.uk>
2026-04-16 07:04:33 +0100

    Allow Artisio staging redirect URLs

--- lib/clients.ts
+++ lib/clients.ts
@@ -65,6 +65,7 @@ const CLIENTS: Record<string, ClientConfig> = {
       "http://127.0.0.1",
       ARTISIO_STAGING_CALLBACK_URL,
       ARTISIO_LIVE_CALLBACK_URL,
+      "https://staging.artisio.co",
       "https://artisio.co",
       "https://www.artisio.co",
     ],
